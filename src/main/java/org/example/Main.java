4150d6a853193a89ebd73e436c6993912dc85008
Eric Schmidt <eric0802@web.de>
2025-03-31 09:49:20 +0200

    define methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public static boolean isPasswordLongEnough(String password) {
+
+    }
+
+    public static boolean doesPasswordContainNumber(String password) {
+
+    }
+
+    public static boolean doesPasswordContainLowercaseAndUppercase(String password) {
+
+    }
+
+    public static boolean isPasswordCommon(String password) {
+
+    }
 }
